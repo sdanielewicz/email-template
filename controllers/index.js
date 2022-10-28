377ae06e04f90542c33fad44ff1c044faa62ed07
sdanielewicz <sdanielewicz@gmail.com>
2022-10-28 12:28:28 -0700

    sucessful render via handlebars

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,30 @@
+// const router = require('express').Router();
+// const apiRoutes = require('./api');
+// const homeRoutes = require('./homeRoutes');
+
+// router.use('/', homeRoutes);
+
+// router.use('/api', apiRoutes);
+
+// // router.use((req, res) => {
+// //   res.send("<h1>Wrong Route!</h1>")
+// // });
+
+// router.use((req, res) => {
+//   res.render('main')
+// });
+
+
+// module.exports = router;
+
+
 const router = require('express').Router();
+
 const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
 
+router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
-
-router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
-});
 
 module.exports = router;
 
